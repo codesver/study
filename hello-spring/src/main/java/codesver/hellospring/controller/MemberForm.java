fc5c999533139e583127ed4e87f9d4cd3484eb1f
codesver <codesver@gmail.com>
2022-06-30 13:13:21 +0900

    [Update] Spring Boot 202206301313

---
+++ hello-spring/src/main/java/codesver/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package codesver.hellospring.controller;
+
+public class MemberForm {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
