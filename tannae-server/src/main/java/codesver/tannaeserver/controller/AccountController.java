8d1e8bc10819440a6841b4cbf6c80721a6d7acf8
codesver <codesver@gmail.com>
2022-06-30 17:18:34 +0900

    [Update] spring-tannae 202206301718

--- tannae-server/src/main/java/codesver/tannaeserver/controller/AccountController.java
+++ tannae-server/src/main/java/codesver/tannaeserver/controller/AccountController.java
@@ -1,15 +1,22 @@
 package codesver.tannaeserver.controller;
 
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import codesver.tannaeserver.domain.Login;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class AccountController {
 
-    @RequestMapping("/account/login")
-    public String login() {
-        JSONObject response = new JSONObject("{'message': 'OK'}");
-        return response.toString();
+    @GetMapping("/account/login")
+    @ResponseBody
+    public String login(Login login) {
+        System.out.println(login.getId() + " " + login.getPw());
+        System.out.println("SUCCESS");
+        JSONObject test = new JSONObject();
+        test.put("message", "success");
+        return test.toString();
     }
 }
