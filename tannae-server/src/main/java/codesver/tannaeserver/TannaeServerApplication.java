a2100a65b8b213886011542d1a840bb798e1a943
codesver <codesver@gmail.com>
2022-06-30 13:28:01 +0900

    [Update] Spring Boot TANNAE 202206301327

---
+++ tannae-server/src/main/java/codesver/tannaeserver/TannaeServerApplication.java
@@ -0,0 +1,13 @@
+package codesver.tannaeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TannaeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TannaeServerApplication.class, args);
+	}
+
+}
---
+++ tannae-server/src/test/java/codesver/tannaeserver/TannaeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package codesver.tannaeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TannaeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
