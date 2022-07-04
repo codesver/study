e68ad4168015015874e799949cf515de24c4d95a
codesver <codesver@gmail.com>
2022-07-04 10:56:05 +0900

    [Update] JDBC

--- hello-spring/src/main/java/codesver/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/codesver/hellospring/service/SpringConfig.java
@@ -1,12 +1,24 @@
 package codesver.hellospring.service;
 
+import codesver.hellospring.repository.JdbcMemberRepository;
+import codesver.hellospring.repository.JdbcTemplateMemberRepository;
 import codesver.hellospring.repository.MemberRepository;
 import codesver.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -14,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
