5f5b93951e358fe612bb664fb0ff71416191fb3b
codesver <codesver@gmail.com>
2022-07-05 11:03:16 +0900

    [Update] AOP 202207051103

--- hello-spring/src/main/java/codesver/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/codesver/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
     @Override
     Optional<Member> findByName(String name);  // 구현체를 자동으로 구현하여 등록
--- hello-spring/src/main/java/codesver/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/codesver/hellospring/service/SpringConfig.java
@@ -1,11 +1,9 @@
 package codesver.hellospring.service;
 
-import codesver.hellospring.repository.*;
+import codesver.hellospring.aop.TimeTraceAOP;
+import codesver.hellospring.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -20,13 +18,12 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
-/*
+    /*
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
         //return new JpaMemberRepository(em);
-
     }*/
 }
