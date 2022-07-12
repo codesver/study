78197ab3141ecfb4c8bc09c7c9cf0c9b45efbd19
codesver <codesver@gmail.com>
2022-07-12 11:13:04 +0900

    [Update] Core 202207121112
    - 회원 도메인 실행과 테스트

---
+++ core/src/main/java/codesver/core/MemberApp.java
@@ -0,0 +1,18 @@
+package codesver.core;
+
+import codesver.core.member.Grade;
+import codesver.core.member.Member;
+import codesver.core.member.MemberService;
+import codesver.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("newMember = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
