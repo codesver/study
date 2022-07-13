379cc90e0b1fc615db7303b506d8a6b35b63a570
codesver <codesver@gmail.com>
2022-07-13 11:29:45 +0900

    [Update] Core 202207131129
    - Spring 전환

--- core/src/main/java/codesver/core/AppConfig.java
+++ core/src/main/java/codesver/core/AppConfig.java
@@ -8,20 +8,27 @@ import codesver.core.member.MemberServiceImpl;
 import codesver.core.member.MemoryMemberRepository;
 import codesver.core.order.OrderService;
 import codesver.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- core/src/main/java/codesver/core/MemberApp.java
+++ core/src/main/java/codesver/core/MemberApp.java
@@ -4,11 +4,17 @@ import codesver.core.member.Grade;
 import codesver.core.member.Member;
 import codesver.core.member.MemberService;
 import codesver.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
