42caab7056af06028f4e89831d5acc910565f217
codesver <codesver@gmail.com>
2022-07-18 10:53:21 +0900

    [Update] Core 202207181053
    - AutoAppConfig
    - 탐색 위치와 스캔 대상

--- core/src/main/java/codesver/core/AutoAppConfig.java
+++ core/src/main/java/codesver/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // default -> basePackagesClasses = AutoAppConfig.class
+        basePackages = "codesver.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
