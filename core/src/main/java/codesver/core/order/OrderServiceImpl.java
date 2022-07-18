47e177c4af2dc9d069c9305e611ebdfda689e1b8
codesver <codesver@gmail.com>
2022-07-18 14:06:32 +0900

    [Update] Core 202207181406
    - Auto DI 방법

--- core/src/main/java/codesver/core/order/OrderServiceImpl.java
+++ core/src/main/java/codesver/core/order/OrderServiceImpl.java
@@ -11,6 +11,22 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
