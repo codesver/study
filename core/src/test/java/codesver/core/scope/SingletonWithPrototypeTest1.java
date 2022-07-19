4027ea0e792ddbab3619e1814093f5b01d1c7d0f
codesver <codesver@gmail.com>
2022-07-19 16:09:03 +0900

    [Update] Core 202207191608
    - Provider

--- core/src/test/java/codesver/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/codesver/core/scope/SingletonWithPrototypeTest1.java
@@ -1,16 +1,17 @@
 package codesver.core.scope;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
     @Test
@@ -36,15 +37,16 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
-    @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
