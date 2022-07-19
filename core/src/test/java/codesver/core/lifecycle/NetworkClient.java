ddfb2ae92a6362dbf461a5dd41fe5637e361433b
codesver <codesver@gmail.com>
2022-07-19 10:45:05 +0900

    [Update] Core 202207191044
    - @Bean 설정 정보에서 지정

--- core/src/test/java/codesver/core/lifecycle/NetworkClient.java
+++ core/src/test/java/codesver/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package codesver.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -26,14 +23,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
