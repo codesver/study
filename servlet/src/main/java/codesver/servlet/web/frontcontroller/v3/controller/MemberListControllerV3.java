1f2f989cc7f2894a6c2da534da7667c4081d86b3
codesver <codesver@gmail.com>
2022-07-26 19:19:15 +0900

    [Update] Servlet 202207261919
    - Version 3

---
+++ servlet/src/main/java/codesver/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,21 @@
+package codesver.servlet.web.frontcontroller.v3.controller;
+
+import codesver.servlet.domain.member.Member;
+import codesver.servlet.domain.member.MemberRepository;
+import codesver.servlet.web.frontcontroller.ModelView;
+import codesver.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+        return mv;
+    }
+}
