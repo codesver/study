ff2dfee1e71a674438c15b8a216d2f84cc258950
codesver <codesver@gmail.com>
2022-07-30 10:58:59 +0900

    [Update] Item-Service 202207301058
    - BasicItemController /add
    - Thymeleaf addForm.html

--- item-service/src/main/java/codesver/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/codesver/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -32,6 +33,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
+    }
+
+    /**
+     * Test Code
+     */
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("ItemA", 10000, 10));
