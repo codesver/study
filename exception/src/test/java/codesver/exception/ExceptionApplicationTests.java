bc26e3cf5d59123d5947432b7bff17e6ccce1d6d
codesver <codesver@gmail.com>
2022-08-04 13:46:04 +0900

    [Create] Exception 202208041345
    - Create Exception project

---
+++ exception/src/test/java/codesver/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package codesver.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
