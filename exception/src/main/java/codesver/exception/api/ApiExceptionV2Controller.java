46a39e77c6dc0b58ffea85808275b09d7aa20023
codesver <codesver@gmail.com>
2022-08-05 23:20:05 +0900

    [Update] Exception 202208052319

--- exception/src/main/java/codesver/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/codesver/exception/api/ApiExceptionV2Controller.java
@@ -1,13 +1,12 @@
 package codesver.exception.api;
 
 import codesver.exception.exception.UserException;
-import codesver.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
