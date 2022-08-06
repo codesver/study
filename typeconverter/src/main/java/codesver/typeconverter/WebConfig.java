65f8c620bc02cbb238d3713ae5d9c24044a38562
codesver <codesver@gmail.com>
2022-08-06 16:50:33 +0900

    [Update] Type-Converter 202208061650
    - Config

--- typeconverter/src/main/java/codesver/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/codesver/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import codesver.typeconverter.converter.IntegerToStringConverter;
 import codesver.typeconverter.converter.IpPortToStringConverter;
 import codesver.typeconverter.converter.StringToIntegerConverter;
 import codesver.typeconverter.converter.StringToIpPortConverter;
+import codesver.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
