897ceb80f5c4d3ac3b27d1bc9b4442aba1f2400c
codesver <codesver@gmail.com>
2022-08-11 13:35:50 +0900

    [Update] JDBC 202208111335
    - Checked Exception

---
+++ jdbc/src/test/java/codesver/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,63 @@
+package codesver.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checkedCatch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checkedThrow() {
+        Assertions.assertThatThrownBy(() -> new Service().callThrow())
+                .isInstanceOf(MyCheckedException.class);
+    }
+
+    /**
+     * Exception 을 상속받은 예외는 check exception 이 된다.
+     */
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는
+     * 예외를 잡아서 처리하거나, 던지거나 둘 중 하나를 필수로 선택해야 한다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * Exception 을 잡아서 처리하는 method
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * Exception 을 던지는 method
+         * @throws MyCheckedException
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+}
