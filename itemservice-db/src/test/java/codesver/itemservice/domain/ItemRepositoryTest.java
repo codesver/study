b55ec33947b4332b92e7b76bb79dced8f4554afe
codesver <codesver@gmail.com>
2022-08-15 14:38:02 +0900

    [Update] ItemService-DB 202208151437
    - PlatformTransactionManager

--- itemservice-db/src/test/java/codesver/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/codesver/itemservice/domain/ItemRepositoryTest.java
@@ -5,9 +5,13 @@ import codesver.itemservice.repository.ItemSearchCond;
 import codesver.itemservice.repository.ItemUpdateDto;
 import codesver.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -19,12 +23,23 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        // Transaction Rollback
+        transactionManager.rollback(status);
     }
 
     @Test
