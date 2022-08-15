3f60076cef94ac525b4e9657a8b329bb29557c17
codesver <codesver@gmail.com>
2022-08-15 15:27:20 +0900

    [Update] ItemService-DB 202208151526
    - h2:mem

--- itemservice-db/src/main/java/codesver/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/codesver/itemservice/ItemServiceApplication.java
@@ -2,15 +2,20 @@ package codesver.itemservice;
 
 import codesver.itemservice.config.*;
 import codesver.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
 @Import(JdbcTemplateV3Config.class)
+@Slf4j
 @SpringBootApplication(scanBasePackages = "codesver.itemservice.web")
 public class ItemServiceApplication {
 
@@ -24,4 +29,16 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+
+/*	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("memory database 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}*/
 }
