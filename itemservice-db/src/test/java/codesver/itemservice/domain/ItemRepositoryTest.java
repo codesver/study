ee2f8514a7a6903bedfd0078ab8dbd897a9206fb
codesver <codesver@gmail.com>
2022-08-15 14:45:49 +0900

    [Update] ItemService-DB 202208151445
    - @Transactional
    - @Commit

--- itemservice-db/src/test/java/codesver/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/codesver/itemservice/domain/ItemRepositoryTest.java
@@ -9,20 +9,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
-
+/*
     @Autowired
     PlatformTransactionManager transactionManager;
     TransactionStatus status;
@@ -31,6 +34,7 @@ class ItemRepositoryTest {
     void beforeEach() {
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
     }
+*/
 
     @AfterEach
     void afterEach() {
@@ -39,7 +43,7 @@ class ItemRepositoryTest {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
         // Transaction Rollback
-        transactionManager.rollback(status);
+//        transactionManager.rollback(status);
     }
 
     @Test
