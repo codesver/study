942eac148aff1572a5c094b27cea9a43cda1c8f7
codesver <codesver@gmail.com>
2022-08-16 18:50:50 +0900

    [Update] ItemService-DB 202208161850
    - QueryDSL
    - Implementation

--- itemservice-db/src/main/java/codesver/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/codesver/itemservice/ItemServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Slf4j
-@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "codesver.itemservice.web")
 public class ItemServiceApplication {
 
