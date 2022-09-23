f6948e22ef195de7859d70f2c57e868647e58243
codesver <codesver@gmail.com>
2022-09-23 20:57:50 +0900

    [Update] JpaBasic 202209232057
    - Inhertance mapping
    - Mapped SuperClass

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -16,14 +17,10 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("user1");
+            member.setCreatedBy("Kim");
+            member.setCreatedDate(LocalDateTime.now());
             em.persist(member);
-
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-            em.persist(team);
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
