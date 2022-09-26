9d450285c71387c04b00084edf2d8a58bf3793db
codesver <codesver@gmail.com>
2022-09-26 17:45:53 +0900

    [Update] JpaShop 202209261745
    - Value Type

--- JpaShop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JpaShop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Book;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,10 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
-            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
