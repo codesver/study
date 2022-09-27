df584b4cb6c295dacb52285a1dcde60bbf6e045c
codesver <codesver@gmail.com>
2022-09-27 09:52:34 +0900

    [Update] JpaBasic 202209270952
    - JPQL Intro

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,6 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.List;
-import java.util.Set;
+import javax.persistence.*;
 
 public class JpaMain {
 
@@ -17,19 +12,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("mMember1");
-            member.setHomeAddress(new Address("city1", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistories().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistories().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
+            Query query = em.createNativeQuery("select * from member where username = 'like'");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
