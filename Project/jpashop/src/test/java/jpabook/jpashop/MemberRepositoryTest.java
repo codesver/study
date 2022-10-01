5842aaff29b4e095f22fc73ead86b92ef5a560ee
codesver <codesver@gmail.com>
2022-10-01 11:29:44 +0900

    [Update] JpaShop 202210011129
    - Create MemberRepositoryTest

---
+++ Project/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("member");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
