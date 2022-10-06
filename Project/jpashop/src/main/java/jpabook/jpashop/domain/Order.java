61fa536fb657121d779879260bfa9b72d88c4f0b
codesver <codesver@gmail.com>
2022-10-06 11:08:43 +0900

    [Update] JpaShop 202210061108
    - Order
      - Add cancel logic

--- Project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -64,4 +64,17 @@ public class Order {
         order.setOrderDate(LocalDateTime.now());
         return order;
     }
+
+    // == Business Logic == //
+    // Cancel order
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
 }
