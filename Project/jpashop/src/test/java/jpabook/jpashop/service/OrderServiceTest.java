9c49cd6505801de49ea0fd6e678c99312a457add
codesver <codesver@gmail.com>
2022-10-07 09:33:36 +0900

    [Update] JpaShop 202210070933
    - Create OrderSericeTest
      - Test order

---
+++ Project/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void order() throws Exception {
+        // given
+        Member member = createMember();
+
+        Book book = createBook("JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        // when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        // then
+        Order foundOrder = orderRepository.findOne(orderId);
+        assertThat(foundOrder.getStatus()).isEqualTo(OrderStatus.ORDER);
+        assertThat(foundOrder.getOrderItems().size()).isEqualTo(1);
+        assertThat(foundOrder.getTotalPrice()).isEqualTo(10000 * orderCount);
+        assertThat(book.getStockQuantity()).isEqualTo(8);
+    }
+
+    private Book createBook(String name, int price, int quantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(quantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("memberA");
+        member.setAddress(new Address("seoul", "river", "123-123"));
+        em.persist(member);
+        return member;
+    }
+}
