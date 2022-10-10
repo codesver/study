33fe7921ed57811d1113a643d019ec01af82481d
codesver <codesver@gmail.com>
2022-10-10 10:53:16 +0900

    [Update] JpaShop 202210101052
    [Update] MemberController
    - update create
    - add BindingResult

--- Project/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -26,7 +27,11 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form) {
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
         Member member = new Member();
@@ -34,6 +39,6 @@ public class MemberController {
         member.setAddress(address);
 
         memberService.join(member);
-        return "redirect:/"
+        return "redirect:/";
     }
 }
