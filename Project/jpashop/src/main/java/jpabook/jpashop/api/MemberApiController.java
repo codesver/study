5e3c2b6d9116737f6c51526e697353bbb8dcf1c9
codesver <codesver@gmail.com>
2022-10-25 14:09:09 +0900

    [JpaShop - Update] MemberApiController 202210251408
    - POST /api/v1/members

--- Project/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,8 +1,14 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,5 +16,18 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 }
