bff9b0c4962afe68a5d5f5cdb89e8ca575a7b4bc
codesver <codesver@gmail.com>
2022-11-01 16:18:04 +0900

    [JpaShop-Update] Version 03

--- Project/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * X to One relation
@@ -29,5 +35,28 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2() {
+        return orderRepository.findAllByString(new OrderSearch())
+                .stream().map(SimpleOrderDTO::new).collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();             // Init LAZY = Search Persistence context -> cannot find then query
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
