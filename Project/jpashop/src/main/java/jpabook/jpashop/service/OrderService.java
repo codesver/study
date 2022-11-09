e775f0a4fe591a839c440d429ffc86b2dfe44bcc
codesver <codesver@gmail.com>
2022-11-09 18:31:28 +0900

    [JpaShop-Update] QueryDSL

--- Project/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -50,7 +50,7 @@ public class OrderService {
     }
 
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAllByString(orderSearch);
+        return orderRepository.findAll(orderSearch);
     }
     
 }
--- Project/jpashop/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -3,12 +3,14 @@ package jpabook.jpashop.service.query;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
+@Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderQueryService {
