1a12eebd54b5fff101f2a0ed743da4f511c90560
codesver <codesver@gmail.com>
2022-11-10 18:57:05 +0900

    test-data-jpa(MemberJpaRepositoryTest): test saving member

--- Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,19 +1,27 @@
 package study.datajpa.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import static lombok.AccessLevel.PROTECTED;
+
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue
     private Long id;
     private String username;
+
+    public Member(String username) {
+        this.username = username;
+    }
 }
