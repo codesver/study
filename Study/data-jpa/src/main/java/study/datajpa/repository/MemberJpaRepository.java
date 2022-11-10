8e1f6f254bc8c60fca09430f8aefb3494529a18a
codesver <codesver@gmail.com>
2022-11-10 18:46:16 +0900

    feat-data-jpa(MemberJpaRepository): add method save

---
+++ Study/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,19 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+}
