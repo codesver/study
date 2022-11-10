10ad5bf78db8d53b118d432655807de3cec06a58
codesver <codesver@gmail.com>
2022-11-10 19:06:17 +0900

    test-data-jpa(MemberRepositoryTest): test saving member

---
+++ Study/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember() {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        // when
+        Member foundMember = memberRepository.findById(savedMember.getId()).get();
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(foundMember).isEqualTo(savedMember);
+    }
+
+}
