d6fd3761dbb4bcafe46e446f9f623ef3dabd1afe
codesver <codesver@gmail.com>
2022-11-10 18:46:54 +0900

    feat-data-jpa(MemberJpaRepository): add method find

--- Study/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ Study/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -16,4 +16,8 @@ public class MemberJpaRepository {
         em.persist(member);
         return member;
     }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
