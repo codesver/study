88394b34f8dd496e144f2af7e5b79382a4de9075
codesver <codesver@gmail.com>
2022-11-14 13:46:16 +0900

    data-jpa: set entity member

--- Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -23,7 +23,7 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
 
