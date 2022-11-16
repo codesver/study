45baaa2b423541092c45d8031c6566ec5e83bece
codesver <codesver@gmail.com>
2022-11-17 08:51:30 +0900

    data-jpa: test pageable

--- Study/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ Study/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,9 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
@@ -160,5 +163,30 @@ class MemberRepositoryTest {
         assertThat(foundMembers.get(0)).isEqualTo(memberA);
         assertThat(foundMember).isEqualTo(memberA);
         assertThat(foundOptionalMember.orElseThrow()).isEqualTo(memberA);
+    }
+
+    @Test
+    void paging() {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        // when
+        Page<Member> pagedMembers = memberRepository.findByAge(age, pageRequest);
+
+        // then
+        List<Member> content = pagedMembers.getContent();
+        long totalElements = pagedMembers.getTotalElements();
+
+        for (Member member : content) {
+            System.out.println("member = " + member);
+        }
+        System.out.println("totalElements = " + totalElements);
     }
 }
