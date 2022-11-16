0d559e34a157756381b9e4169392b6d502541c01
codesver <codesver@gmail.com>
2022-11-16 11:03:09 +0900

    data-jpa: test paging and counting

--- Study/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ Study/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -85,4 +85,26 @@ class MemberJpaRepositoryTest {
         Member foundMember = foundMembers.get(0);
         assertThat(foundMember).isEqualTo(memberA);
     }
+
+    @Test
+    void paging() {
+        // given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> foundMembers = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // then
+        assertThat(foundMembers.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
 }
