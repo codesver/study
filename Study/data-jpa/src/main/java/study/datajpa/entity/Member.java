1fb525b234a22beb8752168d8b1c988f6d913683
codesver <codesver@gmail.com>
2022-11-19 11:48:36 +0900

    data-jpa: implement named entity graph

--- Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Study/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -18,6 +18,7 @@ import static lombok.AccessLevel.PROTECTED;
         name = "Member.findByUsername",
         query = "select m from Member m where m.username = :username"
 )
+@NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
 public class Member {
 
     @Id
