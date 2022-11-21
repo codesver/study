07fa8b277674dede9df1bd4fe63f86be3ff11247
codesver <codesver@gmail.com>
2022-11-21 09:03:42 +0900

    data-jpa: implement lock

--- Study/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ Study/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
 import javax.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
@@ -59,4 +60,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
     Member findReadOnlyByUsername(String username);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
 }
