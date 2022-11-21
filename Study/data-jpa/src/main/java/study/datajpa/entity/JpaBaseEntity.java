8099dd218469de720fa193c56bf383078f09433a
codesver <codesver@gmail.com>
2022-11-21 15:28:54 +0900

    data-jpa: test base entity

--- Study/data-jpa/src/main/java/study/datajpa/entity/JpaBaseEntity.java
+++ Study/data-jpa/src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -1,11 +1,14 @@
 package study.datajpa.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public class JpaBaseEntity {
 
--- Study/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ Study/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,8 +1,10 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.member.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,6 +16,8 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void testEntity() {
@@ -42,7 +46,23 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("member.getTeam() = " + member.getTeam());
         }
-
     }
 
+    @Test
+    void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+        Thread.sleep(100);
+        member.setUsername("member2");
+        em.flush();
+        em.clear();
+
+        // when
+        Member foundMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("foundMember.getCreatedDate() = " + foundMember.getCreatedDate());
+        System.out.println("foundMember.getUpdatedDate() = " + foundMember.getUpdatedDate());
+    }
 }
