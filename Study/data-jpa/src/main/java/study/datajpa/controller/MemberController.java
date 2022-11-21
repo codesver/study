13d5fb3917b42b4dc77b1263bff90de80770736e
codesver <codesver@gmail.com>
2022-11-21 16:51:25 +0900

    data-jpa: implement MemberController

---
+++ Study/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,28 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.member.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable Long id) {
+        Member foundMember = memberRepository.findById(id).get();
+        return foundMember.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
