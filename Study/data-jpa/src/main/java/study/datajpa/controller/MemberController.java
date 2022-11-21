8c65afb5fe009bba7599ac4412af1ffd150e50cf
codesver <codesver@gmail.com>
2022-11-21 16:59:10 +0900

    data-jpa: implement paging controller method

--- Study/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ Study/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,11 @@ public class MemberController {
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        return memberRepository.findAll(pageable);
     }
 
     @PostConstruct
