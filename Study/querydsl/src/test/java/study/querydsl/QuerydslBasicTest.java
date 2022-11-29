745e85b311862b10ce001812c814d9e2125625b0
codesver <codesver@gmail.com>
2022-11-29 14:29:32 +0900

    querydsl-test: test complex case

--- Study/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Study/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -368,6 +369,21 @@ public class QuerydslBasicTest {
                 .select(member.age
                         .when(10).then("열살")
                         .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String foundMember : foundMembers) {
+            System.out.println("foundMember = " + foundMember);
+        }
+    }
+
+    @Test
+    void complexCase() {
+        List<String> foundMembers = query
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
                         .otherwise("기타"))
                 .from(member)
                 .fetch();
