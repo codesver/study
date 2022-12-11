1084fa5d4aee67214590458b08db56bf4d2e4f50
codesver <codesver@gmail.com>
2022-12-12 08:41:09 +0900

    querydsl-test: test member jpa repository

---
+++ Study/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void basicTest() {
+        Member memberA = new Member("memberA", 10);
+        memberJpaRepository.save(memberA);
+
+        Member foundMember = memberJpaRepository.findById(memberA.getId()).get();
+        assertThat(foundMember).isEqualTo(memberA);
+
+        List<Member> foundAllMembers = memberJpaRepository.findAll();
+        assertThat(foundAllMembers).containsExactly(memberA);
+
+        List<Member> foundMemberA = memberJpaRepository.findByUsername("memberA");
+        assertThat(foundMemberA).containsExactly(memberA);
+    }
+}
