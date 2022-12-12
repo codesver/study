462da8320f648c6dddbc19f332bbf7331eee2142
codesver <codesver@gmail.com>
2022-12-12 09:55:16 +0900

    querydsl-refactor: inject query factory bean

--- Study/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ Study/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,26 +1,22 @@
 package study.querydsl.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
-import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QMember.member;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJpaRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
-
-    public MemberJpaRepository(EntityManager em) {
-        this.em = em;
-        this.queryFactory = new JPAQueryFactory(em);
-    }
 
     public void save(Member member) {
         em.persist(member);
@@ -43,9 +39,9 @@ public class MemberJpaRepository {
 
     public List<Member> findByUsername(String username) {
         return em.createQuery(
-                "select m " +
-                        "from Member m " +
-                        "where m.username = :username", Member.class)
+                        "select m " +
+                                "from Member m " +
+                                "where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
     }
