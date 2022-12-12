77a65d847e9f28dd37631749381b0a82b7886d27
codesver <codesver@gmail.com>
2022-12-12 10:22:19 +0900

    querydsl-test: test searching by builder

--- Study/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ Study/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,10 +1,14 @@
 package study.querydsl.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDTO;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -49,5 +53,31 @@ class MemberJpaRepositoryTest {
 
         List<Member> foundMemberA = memberJpaRepository.findByUsernameQuerydsl("memberA");
         assertThat(foundMemberA).containsExactly(memberA);
+    }
+
+    @Test
+    void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDTO> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("memberD");
     }
 }
