f9120ac80b9d50eb64c794952f1556dc7f70a65d
codesver <codesver@gmail.com>
2022-12-13 09:09:40 +0900

    querydsl-test: test simple page searching

--- Study/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ Study/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,8 @@ package study.querydsl.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDTO;
@@ -22,7 +24,7 @@ class MemberRepositoryTest {
     EntityManager em;
 
     @Autowired
-    MemberJpaRepository memberRepository;
+    MemberRepository memberRepository;
 
     @Test
     void basicTest() {
@@ -63,5 +65,32 @@ class MemberRepositoryTest {
         List<MemberTeamDTO> result = memberRepository.search(condition);
 
         assertThat(result).extracting("username").containsExactly("memberD");
+    }
+
+    @Test
+    void searchPageSimpleTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        Page<MemberTeamDTO> result = memberRepository.searchPageSimple(condition, pageRequest);
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent())
+                .extracting("username")
+                .containsExactly("memberA", "memberB", "memberC");
     }
 }
