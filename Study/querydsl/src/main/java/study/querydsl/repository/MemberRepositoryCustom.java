30ce5a915e9fa11521ef56c4dd160d757b20e662
codesver <codesver@gmail.com>
2022-12-13 09:01:06 +0900

    querydsl-feat: add member custom methods

--- Study/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ Study/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -3,9 +3,14 @@ package study.querydsl.repository;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDTO;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MemberRepositoryCustom {
 
     List<MemberTeamDTO> search(MemberSearchCondition condition);
+
+    List<MemberTeamDTO> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+
+    List<MemberTeamDTO> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
