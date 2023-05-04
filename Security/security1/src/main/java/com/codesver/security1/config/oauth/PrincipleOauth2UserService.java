539ab887832f77795dbd2e1a5c26c3e8f87ab011
codesver <codesver@gmail.com>
2023-05-04 20:20:26 +0900

    Chapter 11 : Naver Login

--- Security/security1/src/main/java/com/codesver/security1/config/oauth/PrincipleOauth2UserService.java
+++ Security/security1/src/main/java/com/codesver/security1/config/oauth/PrincipleOauth2UserService.java
@@ -1,20 +1,23 @@
 package com.codesver.security1.config.oauth;
 
-import com.codesver.security1.config.auth.PrincipleDetails;
 import com.codesver.security1.config.CustomBCryptPasswordEncoder;
+import com.codesver.security1.config.auth.PrincipleDetails;
 import com.codesver.security1.config.oauth.provider.FacebookUserInfo;
 import com.codesver.security1.config.oauth.provider.GoogleUserInfo;
+import com.codesver.security1.config.oauth.provider.NaverUserInfo;
 import com.codesver.security1.config.oauth.provider.OAuth2UserInfo;
 import com.codesver.security1.model.User;
 import com.codesver.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -35,10 +38,19 @@ public class PrincipleOauth2UserService extends DefaultOAuth2UserService {
         log.info("User Profile Info = {}", oAuth2User.getAttributes());
 
         OAuth2UserInfo oAuth2UserInfo = null;
-        if (userRequest.getClientRegistration().getRegistrationId().equals("google"))
-            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-        else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook"))
-            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+
+        switch (provider) {
+            case "google":
+                oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+                break;
+            case "facebook":
+                oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+                break;
+            case "naver":
+                oAuth2UserInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
+                break;
+        }
 
         assert oAuth2UserInfo != null;
         User user = new User(oAuth2UserInfo);
