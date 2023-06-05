806c59fa96f5fd10d4cbcde2a2db955e1719b68c
codesver <codesver@gmail.com>
2023-06-05 14:56:01 +0900

    Chapter 01 : 코틀린에서 변수를 다루는 방법

---
+++ Kotlin/KotlinBasic/src/main/kotlin/CH01Variable.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // var and val
+    var mutableNumber: Long
+    val immutableNumber: Long
+
+    // Nullable
+    var nullableLongNumber: Long? = 1_000L
+
+    // Instance
+    val person = Person("codesver")
+}
+
+class Person(name: String);
