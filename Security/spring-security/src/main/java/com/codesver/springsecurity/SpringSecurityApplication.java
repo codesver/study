e6f3c392a048b5d06dcdb5a743c5c03f4255859c
codesver <codesver@gmail.com>
2023-06-21 15:27:29 +0900

    init project
    Initialize project for studying spring security

---
+++ Security/spring-security/src/main/java/com/codesver/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.codesver.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
